4d17f2fcd7c69a698d1a6b1fd66ede325eb6b2f3
Fernanda Araújo <fernandacomun@gmail.com>
2026-02-10 09:13:50 -0300

    commit

--- 3-periodo/banco de dados II/sistema-rh/frontend/src/components/ui/BotaoFechar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function BotaoFechar({ aoClicar }) {
-  return (
-    <button onClick={aoClicar} className="absolute top-3 right-3 text-gray-500 hover:text-black dark:hover:text-white" >
-      ✕
-    </button>
-  );
-}
